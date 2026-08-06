ecf2866b68c8fed54b04627408cfdc146611ffeb
saulosilva2809 <saulosilva2809@users.noreply.github.com>
2026-08-06 08:57:34 -0300

    feat: sets up the environment and creates unit tests for CategoryService

--- category/src/main/resources/db/migration/V1__create-category-table.sql
+++ category/src/main/resources/db/migration/V1__create-category-table.sql
@@ -1,10 +1,8 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
 CREATE TABLE category (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     account_id UUID NOT NULL,
     name VARCHAR(255) NOT NULL,
     type VARCHAR(50) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP
-)
+);
