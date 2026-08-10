c49c482490c77271a956171dd6899e0b9cab9697
saulosilva2809 <saulosilva2809@users.noreply.github.com>
2026-08-10 09:25:59 -0300

    feat: fixes tests after changes in the service and repository

--- category/src/test/java/com/emmanuelfinance/category/CategoryIntegrationTest.java
+++ category/src/test/java/com/emmanuelfinance/category/CategoryIntegrationTest.java
@@ -2,8 +2,10 @@ package com.emmanuelfinance.category;
 
 import com.emmanuelfinance.category.dto.CategoryFIltersDTO;
 import com.emmanuelfinance.category.enums.TypeEnum;
+import com.emmanuelfinance.shared.security.SecurityUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
@@ -15,6 +17,7 @@ import org.springframework.test.context.ActiveProfiles;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -23,27 +26,37 @@ class CategoryIntegrationTest {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Mock
+    private SecurityUtils securityUtils;
+
+    private UUID userId;
     private UUID account1;
     private UUID account2;
 
     @BeforeEach
     void setUp() {
+        when(securityUtils.getCurrentUserId()).thenReturn(UUID.randomUUID());
+        userId = securityUtils.getCurrentUserId();
+
         categoryRepository.deleteAll();
 
         account1 = UUID.randomUUID();
         account2 = UUID.randomUUID();
 
         Category cat1 = new Category();
+        cat1.setUserId(userId);
         cat1.setAccountId(account1);
         cat1.setName("Salário Mensal");
         cat1.setType(TypeEnum.INCOME);
 
         Category cat2 = new Category();
+        cat2.setUserId(userId);
         cat2.setAccountId(account1);
         cat2.setName("Salário Funcionários");
         cat2.setType(TypeEnum.EXPENSE);
 
         Category cat3 = new Category();
+        cat3.setUserId(userId);
         cat3.setAccountId(account2);
         cat3.setName("Investimentos");
         cat3.setType(TypeEnum.INCOME);
@@ -61,7 +74,7 @@ class CategoryIntegrationTest {
                 TypeEnum.INCOME
         );
         Pageable pageable = PageRequest.of(0, 10);
-        Specification<Category> spec = CategorySpecification.withFilter(fIltersDTO);
+        Specification<Category> spec = CategorySpecification.withFilter(fIltersDTO, userId);
 
         Page<Category> result = categoryRepository.findAll(spec, pageable);
 
@@ -80,7 +93,7 @@ class CategoryIntegrationTest {
                 TypeEnum.EXPENSE
         );
         Pageable pageable = PageRequest.of(0, 10);
-        Specification<Category> spec = CategorySpecification.withFilter(fIltersDTO);
+        Specification<Category> spec = CategorySpecification.withFilter(fIltersDTO, userId);
 
         Page<Category> result = categoryRepository.findAll(spec, pageable);
 
