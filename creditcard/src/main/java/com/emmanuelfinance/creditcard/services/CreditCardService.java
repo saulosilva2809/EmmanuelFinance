62beb7dcac97847657886576b0908eda7d8b5485
saulosilva2809 <saulosilva2809@users.noreply.github.com>
2026-08-30 15:21:24 -0300

    feat: separate service and validations

--- creditcard/src/main/java/com/emmanuelfinance/creditcard/services/CreditCardService.java
+++ creditcard/src/main/java/com/emmanuelfinance/creditcard/services/CreditCardService.java
@@ -5,15 +5,9 @@ import com.emmanuelfinance.creditcard.dto.CreateCreditCardDTO;
 import com.emmanuelfinance.creditcard.dto.CreditCardFiltersDTO;
 import com.emmanuelfinance.creditcard.dto.ResponseCreditCardDTO;
 import com.emmanuelfinance.creditcard.dto.UpdateCreditCardDTO;
-import com.emmanuelfinance.creditcard.exceptions.CheckCardAndAccountBankError;
-import com.emmanuelfinance.creditcard.exceptions.RestoreCreditCardError;
 import com.emmanuelfinance.shared.annotation.WithDeletedFilter;
 import com.emmanuelfinance.shared.dto.PageResponseDTO;
-import com.emmanuelfinance.shared.enums.BanksEnum;
-import com.emmanuelfinance.shared.modules.account.AccountClientCacheService;
 import com.emmanuelfinance.shared.modules.account.AccountOwnershipValidator;
-import com.emmanuelfinance.shared.modules.account.dto.AccountSummaryDTO;
-import com.emmanuelfinance.shared.modules.account.dto.AccountSummaryInternalDTO;
 import com.emmanuelfinance.shared.security.SecurityUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -29,63 +23,31 @@ import java.util.UUID;
 public class CreditCardService {
 
     private final AccountOwnershipValidator accountOwnershipValidator;
-    private final AccountClientCacheService accountClientCacheService;
     private final SecurityUtils securityUtils;
     private final CreditCardRepository creditCardRepository;
     private final CreditCardMapper cardMapper;
     private final CreditCardSelector cardSelector;
-
-    private ResponseCreditCardDTO cardAsDTO(CreditCard data) {
-        AccountSummaryDTO account = accountClientCacheService
-                .getAccountSummaryById(data.getAccountId());
-
-        return new ResponseCreditCardDTO(
-                data.getId(),
-                account,
-                data.getName(),
-                data.getBank(),
-                data.getCreditLimit(),
-                data.getClosingDay(),
-                data.getDueDay(),
-                data.getCreatedAt(),
-                data.getUpdatedAt(),
-                data.isDeleted()
-        );
-    }
-
-    private void checkCardAndAccountBank(BanksEnum cardBank, UUID accountId) {
-        AccountSummaryInternalDTO account = accountClientCacheService.getInternalAccountById(accountId);
-
-        if (!cardBank.equals(account.bank())) {
-            throw new CheckCardAndAccountBankError();
-        }
-    }
+    private final CreditCardValidatorService creditCardValidatorService;
 
     public ResponseCreditCardDTO create(CreateCreditCardDTO data) {
         UUID userId = securityUtils.getCurrentUserId();
 
-        accountOwnershipValidator.validate(data.accountId());
-        checkCardAndAccountBank(data.bank(), data.accountId());
+        creditCardValidatorService.validateCreation(data);
 
-        CreditCard creditCard = new CreditCard();
+        CreditCard creditCard = cardMapper.toEntity(data);
 
         creditCard.setUserId(userId);
-        creditCard.setAccountId(data.accountId());
-        creditCard.setName(data.name());
-        creditCard.setBank(data.bank());
         Optional.ofNullable(data.creditLimit())
                 .ifPresent(creditLimit -> creditCard.setCreditLimit(creditLimit));
-        creditCard.setClosingDay(data.closingDay());
-        creditCard.setDueDay(data.dueDay());
 
         CreditCard savedCreditCard = creditCardRepository.save(creditCard);
 
-        return cardAsDTO(savedCreditCard);
+        return cardMapper.toResponseDTO(savedCreditCard);
     }
 
     public ResponseCreditCardDTO view(UUID cardId) {
         CreditCard creditCard = cardSelector.getCreditCardById(cardId);
-        return cardAsDTO(creditCard);
+        return cardMapper.toResponseDTO(creditCard);
     }
 
     @WithDeletedFilter()
@@ -98,7 +60,7 @@ public class CreditCardService {
                 pageable
         );
 
-        Page<ResponseCreditCardDTO> dtoPage = page.map(this::cardAsDTO);
+        Page<ResponseCreditCardDTO> dtoPage = page.map(cardMapper::toResponseDTO);
 
         return PageResponseDTO.from(dtoPage);
     }
@@ -113,7 +75,7 @@ public class CreditCardService {
                 pageable
         );
 
-        Page<ResponseCreditCardDTO> dtoPage = page.map(this::cardAsDTO);
+        Page<ResponseCreditCardDTO> dtoPage = page.map(cardMapper::toResponseDTO);
 
         return PageResponseDTO.from(dtoPage);
     }
@@ -128,7 +90,7 @@ public class CreditCardService {
         cardMapper.updateCreditCardFromDTO(data, creditCard);
         CreditCard updatedCard = creditCardRepository.save(creditCard);
 
-        return cardAsDTO(updatedCard);
+        return cardMapper.toResponseDTO(updatedCard);
     }
 
     public void delete(UUID cardId) {
@@ -139,9 +101,7 @@ public class CreditCardService {
     public void restore(UUID cardId) {
         CreditCard creditCard = cardSelector.getCreditCardByIdIncludingDeleted(cardId);
 
-        if (!creditCard.isDeleted()) {
-            throw new RestoreCreditCardError();
-        }
+        creditCardValidatorService.validateRestoration(creditCard);
 
         creditCard.setDeleted(false);
         creditCardRepository.save(creditCard);
